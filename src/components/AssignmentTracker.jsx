fd984b4036aa29f27712272d924d015322cb4f20
Oreofe05 <olukoladeoreofe24@gmail.com>
2026-06-22 15:05:05 +0100

    Style correction

--- src/components/AssignmentTracker.jsx
+++ src/components/AssignmentTracker.jsx
@@ -301,9 +301,9 @@ function AssignmentTracker() {
         >
           Add Assignment
         </button>
-
+        
       </div>
-
+      <br />
       {/* Assignment summary cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
 
